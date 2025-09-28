a9ea1ef9c5011858c2f538daa0e68bce10afc224
Syifa Alifiya <145785015+cheepi@users.noreply.github.com>
2025-09-28 18:07:08 +0700

    reset

--- middleware/roomBooking.js
+++
@@ -1,8 +0,0 @@
-// Middleware dummy untuk validasi booking waktu
-function validateBookingTime(req, res, next) {
-  // nanti bisa diisi logika beneran (cek overlap, jam buka, dll.)
-  console.log("✅ validateBookingTime kepanggil");
-  next();
-}
-
-module.exports = { validateBookingTime };
--- utils/fines.js
+++
@@ -1,3 +0,0 @@
-// utils/fines.js
-// backward-compat wrapper (some files still require '../utils/fines')
-module.exports = require('./fineCalculator');
